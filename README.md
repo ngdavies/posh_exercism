dcba4a122c1ba90e1cd64819c231abbfcfc5c230
Nigel Davies <ngdavies@hotmail.com>
2021-10-28 13:49:06 +0100

    Recreate this repo to allign with github creation

---
+++ README.md
@@ -0,0 +1,1 @@
+A repo of powershell exercises from Exercism (and Execism/basn in powershell perhaps).
---
+++ hello-world/HelloWorld.ps1
@@ -0,0 +1,24 @@
+function Get-HelloWorld {
+    <#
+    .SYNOPSIS
+    Outputs "Hello, World!"
+    
+    .DESCRIPTION
+    Output "Hello, World!" or "Hello, <Name>!" if Name is supplied to the CmdLet.
+    
+    .PARAMETER person
+    Name of the person to display. If not supplied, Hello World should be displayed
+    
+    .EXAMPLE
+    Get-HelloWorld
+    #>
+    # param([string]$person = "World")
+
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $Name = "World"
+	)
+
+    return "Hello, $Name!"
+}
