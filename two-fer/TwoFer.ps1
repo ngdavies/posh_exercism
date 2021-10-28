e6b31a47321868fb4fd63ba5661839ef06e9d189
Nigel Davies <ngdavies@hotmail.com>
2021-10-28 14:42:26 +0100

    Two-fer tests ppass

---
+++ two-fer/TwoFer.ps1
@@ -0,0 +1,33 @@
+Function Get-TwoFer(){
+    <#
+    .SYNOPSIS
+    "Two-fer" is short for two for one. One for you and one for me.
+    
+    .DESCRIPTION
+    If the given name is "Alice", the result should be "One for Alice, one for me."
+    If no name is given, the result should be "One for you, one for me."
+    
+    .PARAMETER Name
+    The name to use.
+    
+    .EXAMPLE
+    Get-TwoFer
+
+    Will return: One for you, one for me
+
+    .EXAMPLE
+    Get-TwoFer -Name Alice
+    
+    Will return: One for Alice, one for me
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false, Position=0)]
+        [string]$Name = "you"
+    )
+    if (-not $Name) {
+        $Name = "you"
+    } 
+    return "One for $Name, one for me"
+}
