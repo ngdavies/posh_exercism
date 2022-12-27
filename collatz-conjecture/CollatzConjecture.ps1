ad18ffb9c87c3329e42d1b069ba980ffc3f70388
Nigel Davies <ngdavies@hotmail.com>
2022-12-27 15:45:30 +0000

    Collatz tests passing

--- collatz-conjecture/CollatzConjecture.ps1
+++ collatz-conjecture/CollatzConjecture.ps1
@@ -4,7 +4,8 @@ Function Invoke-CollatzConjecture() {
     Calculate the number of steps to reach 1 using the Collatz conjecture.
 
     .DESCRIPTION
-    Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+    Take any positive integer n. If n is even, divide n by 2 to get n / 2.
+    If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
 
     .PARAMETER Number
     The number to perform the Collatz Conjecture function on.
@@ -17,13 +18,19 @@ Function Invoke-CollatzConjecture() {
         [Int64]$Number
     )
 
-    $Count = 0
-    if ($Number -lt 1) {
-        throw "error: Only positive numbers are allowed"
-    } 
-    while ($Number -gt 1) {
-        $Number = ($Number %2 -eq 0) ? $Number / 2 : 3 * $Number + 1
-        $Count += 1
+    if ($Number -le 0) {
+        Throw "error: Only positive numbers are allowed"
     }
-    return $Count
+
+    $Steps = 0
+    while ($Number -gt 1) {
+        if ($Number % 2) {
+            $Number *= 3
+            $Number += 1
+        } else {
+            $Number /= 2
+        }
+        $Steps++ 
+    }
+    return $Steps
 }
